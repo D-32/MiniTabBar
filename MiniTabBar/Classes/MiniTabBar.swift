f273b8de24fb827653c77b17a89aacf37dc64502
Dylan Marriott <info@d-32.com>
2017-03-29 00:14:03 +0200

    Formatting

--- MiniTabBar/Classes/MiniTabBar.swift
+++ MiniTabBar/Classes/MiniTabBar.swift
@@ -10,104 +10,104 @@ import Foundation
 import UIKit
 
 public class MiniTabBarItem {
-  var title: String
-  var icon: UIImage
-  public init(title: String, icon:UIImage) {
-    self.title = title
-    self.icon = icon
-  }
+    var title: String
+    var icon: UIImage
+    public init(title: String, icon:UIImage) {
+        self.title = title
+        self.icon = icon
+    }
 }
 
 public protocol MiniTabBarDelegate: class {
-  func tabSelected(_ index: Int)
+    func tabSelected(_ index: Int)
 }
 
 public class MiniTabBar: UIView {
-
-  public weak var delegate: MiniTabBarDelegate?
-  public let keyLine = UIView()
-  public override var tintColor: UIColor! {
-    didSet {
-      for itv in self.itemViews {
-        itv.tintColor = self.tintColor
-      }
+    
+    public weak var delegate: MiniTabBarDelegate?
+    public let keyLine = UIView()
+    public override var tintColor: UIColor! {
+        didSet {
+            for itv in self.itemViews {
+                itv.tintColor = self.tintColor
+            }
+        }
     }
-  }
-  public var font: UIFont? {
-    didSet {
-      for itv in self.itemViews {
-        itv.font = self.font
-      }
+    public var font: UIFont? {
+        didSet {
+            for itv in self.itemViews {
+                itv.font = self.font
+            }
+        }
     }
-  }
-
-  private let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .extraLight)) as UIVisualEffectView
-  public var backgroundBlurEnabled: Bool = true {
-    didSet {
-      self.visualEffectView.isHidden = !self.backgroundBlurEnabled
+    
+    private let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .extraLight)) as UIVisualEffectView
+    public var backgroundBlurEnabled: Bool = true {
+        didSet {
+            self.visualEffectView.isHidden = !self.backgroundBlurEnabled
+        }
     }
-  }
-
-  public var itemWidth: CGFloat = 100.0
-
-  fileprivate var itemViews = [MiniTabBarItemView]()
-  fileprivate var currentSelectedIndex: Int?
-
-  public init(items: [MiniTabBarItem]) {
-    super.init(frame: CGRect.zero)
-
-    self.backgroundColor = UIColor(white: 1.0, alpha: 0.8)
-
-    self.addSubview(visualEffectView)
-
-    keyLine.backgroundColor = UIColor(white: 0.9, alpha: 1.0)
-    self.addSubview(keyLine)
-
-    var i = 0
-    for item in items {
-      let itemView = MiniTabBarItemView(title: item.title, icon: item.icon)
-      itemView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(MiniTabBar.itemTapped(_:))))
-      self.itemViews.append(itemView)
-      self.addSubview(itemView)
-      i += 1
+    
+    public var itemWidth: CGFloat = 100.0
+    
+    fileprivate var itemViews = [MiniTabBarItemView]()
+    fileprivate var currentSelectedIndex: Int?
+    
+    public init(items: [MiniTabBarItem]) {
+        super.init(frame: CGRect.zero)
+        
+        self.backgroundColor = UIColor(white: 1.0, alpha: 0.8)
+        
+        self.addSubview(visualEffectView)
+        
+        keyLine.backgroundColor = UIColor(white: 0.9, alpha: 1.0)
+        self.addSubview(keyLine)
+        
+        var i = 0
+        for item in items {
+            let itemView = MiniTabBarItemView(title: item.title, icon: item.icon)
+            itemView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(MiniTabBar.itemTapped(_:))))
+            self.itemViews.append(itemView)
+            self.addSubview(itemView)
+            i += 1
+        }
+        
+        self.selectItem(0, animated: false)
     }
-
-    self.selectItem(0, animated: false)
-  }
-
-  required public init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  public override func layoutSubviews() {
-    super.layoutSubviews()
-    visualEffectView.frame = self.bounds
-    keyLine.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: 1)
-
-    let spacing = Int((frame.size.width - (CGFloat(self.itemViews.count) * itemWidth)) / CGFloat(self.itemViews.count + 1))
-    for (i, itemView) in self.itemViews.enumerated() {
-      let x = CGFloat(spacing * (i+1)) + (itemWidth * CGFloat(i))
-      itemView.frame = CGRect(x: x, y: 0, width: itemWidth, height: frame.size.height)
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-  }
-
-  func itemTapped(_ gesture: UITapGestureRecognizer) {
-    let itemView = gesture.view as! MiniTabBarItemView
-    let selectedIndex = self.itemViews.index(of: itemView)!
-    self.selectItem(selectedIndex)
-  }
-
-  public func selectItem(_ selectedIndex: Int, animated: Bool = true) {
-    if (selectedIndex == self.currentSelectedIndex) {
-      return
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        visualEffectView.frame = self.bounds
+        keyLine.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: 1)
+        
+        let spacing = Int((frame.size.width - (CGFloat(self.itemViews.count) * itemWidth)) / CGFloat(self.itemViews.count + 1))
+        for (i, itemView) in self.itemViews.enumerated() {
+            let x = CGFloat(spacing * (i+1)) + (itemWidth * CGFloat(i))
+            itemView.frame = CGRect(x: x, y: 0, width: itemWidth, height: frame.size.height)
+        }
     }
-    self.currentSelectedIndex = selectedIndex
-
-    for (index, v) in self.itemViews.enumerated() {
-      v.setSelected((index == selectedIndex), animated: animated)
+    
+    func itemTapped(_ gesture: UITapGestureRecognizer) {
+        let itemView = gesture.view as! MiniTabBarItemView
+        let selectedIndex = self.itemViews.index(of: itemView)!
+        self.selectItem(selectedIndex)
     }
-
-    self.delegate?.tabSelected(selectedIndex)
-  }
+    
+    public func selectItem(_ selectedIndex: Int, animated: Bool = true) {
+        if (selectedIndex == self.currentSelectedIndex) {
+            return
+        }
+        self.currentSelectedIndex = selectedIndex
+        
+        for (index, v) in self.itemViews.enumerated() {
+            v.setSelected((index == selectedIndex), animated: animated)
+        }
+        
+        self.delegate?.tabSelected(selectedIndex)
+    }
 }
 
--- MiniTabBar/Classes/MiniTabBarItemView.swift
+++ MiniTabBar/Classes/MiniTabBarItemView.swift
@@ -10,75 +10,75 @@ import Foundation
 import UIKit
 
 class MiniTabBarItemView: UIView {
-  let titleLabel = UILabel()
-  let iconView = UIImageView()
-
-  private var selected = false
-
-  override var tintColor: UIColor! {
-    didSet {
-      if self.selected {
-        self.iconView.tintColor = self.tintColor
-      }
-      self.titleLabel.textColor = self.tintColor
+    let titleLabel = UILabel()
+    let iconView = UIImageView()
+    
+    private var selected = false
+    
+    override var tintColor: UIColor! {
+        didSet {
+            if self.selected {
+                self.iconView.tintColor = self.tintColor
+            }
+            self.titleLabel.textColor = self.tintColor
+        }
     }
-  }
-  private let defaultFont = UIFont.systemFont(ofSize: 12)
-  var font: UIFont? {
-    didSet {
-      self.titleLabel.font = self.font ?? defaultFont
+    private let defaultFont = UIFont.systemFont(ofSize: 12)
+    var font: UIFont? {
+        didSet {
+            self.titleLabel.font = self.font ?? defaultFont
+        }
     }
-  }
-
-  init(title: String, icon: UIImage) {
-    super.init(frame: CGRect.zero)
-    titleLabel.text = title
-    titleLabel.font = self.defaultFont
-    titleLabel.textColor = self.tintColor
-    titleLabel.textAlignment = .center
-    self.addSubview(titleLabel)
-
-    iconView.image = icon.withRenderingMode(.alwaysTemplate)
-    self.addSubview(iconView)
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  override func layoutSubviews() {
-    super.layoutSubviews()
-    titleLabel.frame = CGRect(x: 0, y: self.frame.height, width: self.frame.width, height: 14)
-    iconView.frame = CGRect(x: self.frame.width / 2 - 13, y: 12, width: 26, height: 20)
-  }
-
-  func setSelected(_ selected: Bool, animated: Bool = true) {
-    self.selected = selected
-    self.iconView.tintColor = selected ? self.tintColor : UIColor(white: 0.6, alpha: 1.0)
-
-    if (animated && selected) {
-      /*
-       ICON
-       */
-      UIView.animate(withDuration: 0.15, delay: 0, options: .curveEaseInOut, animations: {
-        self.iconView.frame.origin.y = 5
-      }, completion: { finished in
-        UIView.animate(withDuration: 0.4, delay: 0.5, options: UIViewAnimationOptions(), animations: {
-          self.iconView.frame.origin.y = 12
-        })
-      })
-
-
-      /*
-       TEXT
-       */
-      UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions(), animations: {
-        self.titleLabel.frame.origin.y = 28
-      }, completion: { finished in
-        UIView.animate(withDuration: 0.2, delay: 0.5, options: UIViewAnimationOptions(), animations: {
-          self.titleLabel.frame.origin.y = self.frame.size.height
-        })
-      })
+    
+    init(title: String, icon: UIImage) {
+        super.init(frame: CGRect.zero)
+        titleLabel.text = title
+        titleLabel.font = self.defaultFont
+        titleLabel.textColor = self.tintColor
+        titleLabel.textAlignment = .center
+        self.addSubview(titleLabel)
+        
+        iconView.image = icon.withRenderingMode(.alwaysTemplate)
+        self.addSubview(iconView)
     }
-  }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        titleLabel.frame = CGRect(x: 0, y: self.frame.height, width: self.frame.width, height: 14)
+        iconView.frame = CGRect(x: self.frame.width / 2 - 13, y: 12, width: 26, height: 20)
+    }
+    
+    func setSelected(_ selected: Bool, animated: Bool = true) {
+        self.selected = selected
+        self.iconView.tintColor = selected ? self.tintColor : UIColor(white: 0.6, alpha: 1.0)
+        
+        if (animated && selected) {
+            /*
+             ICON
+             */
+            UIView.animate(withDuration: 0.15, delay: 0, options: .curveEaseInOut, animations: {
+                self.iconView.frame.origin.y = 5
+            }, completion: { finished in
+                UIView.animate(withDuration: 0.4, delay: 0.5, options: UIViewAnimationOptions(), animations: {
+                    self.iconView.frame.origin.y = 12
+                })
+            })
+            
+            
+            /*
+             TEXT
+             */
+            UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions(), animations: {
+                self.titleLabel.frame.origin.y = 28
+            }, completion: { finished in
+                UIView.animate(withDuration: 0.2, delay: 0.5, options: UIViewAnimationOptions(), animations: {
+                    self.titleLabel.frame.origin.y = self.frame.size.height
+                })
+            })
+        }
+    }
 }
