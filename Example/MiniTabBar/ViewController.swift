5ae643266c6f423fa7fa9abf0b0710d2af87f613
Dylan Marriott <info@d-32.com>
2017-03-30 15:56:42 +0200

    Custom view example

--- Example/MiniTabBar/ViewController.swift
+++ Example/MiniTabBar/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController, MiniTabBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.createSimpleTabBar()
+        //self.createCustomItemTabBar()
+    }
+    
+    private func createSimpleTabBar() {
         var items = [MiniTabBarItem]()
         items.append(MiniTabBarItem(title: "Home", icon: #imageLiteral(resourceName: "tab1")))
         items.append(MiniTabBarItem(title: "Profile", icon: #imageLiteral(resourceName: "tab2")))
@@ -23,9 +27,33 @@ class ViewController: UIViewController, MiniTabBarDelegate {
         tabBar.frame = CGRect(x: 0, y: self.view.frame.height - 44, width: self.view.frame.width, height: 44)
         self.view.addSubview(tabBar)
     }
-
-  func tabSelected(_ index: Int) {
-    print("Selected tab: ", index)
-  }
+    
+    private func createCustomItemTabBar() {
+        var items = [MiniTabBarItem]()
+        items.append(MiniTabBarItem(title: "Home", icon: #imageLiteral(resourceName: "tab1")))
+        
+        let customButton = UIButton()
+        customButton.backgroundColor = UIColor.orange
+        customButton.layer.cornerRadius = 4
+        customButton.frame.size = CGSize(width: 50, height: 50)
+        customButton.addTarget(self, action: #selector(customButtonTapped), for: .touchUpInside)
+        let customItem = MiniTabBarItem(customView: customButton, offset: UIOffset(horizontal: 0, vertical: -10))
+        customItem.selectable = false
+        items.append(customItem)
+        
+        items.append(MiniTabBarItem(title: "Tickets", icon: #imageLiteral(resourceName: "tab3")))
+        let tabBar = MiniTabBar(items: items)
+        tabBar.delegate = self
+        tabBar.frame = CGRect(x: 0, y: self.view.frame.height - 44, width: self.view.frame.width, height: 44)
+        self.view.addSubview(tabBar)
+    }
+    
+    func tabSelected(_ index: Int) {
+        print("Selected tab: ", index)
+    }
+    
+    @objc private func customButtonTapped() {
+        print("Custom button tapped")
+    }
 }
 
